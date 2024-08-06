f18651b0b3ae9ba6c5c33579b51ceae22b1355bf
Jérôme SAVAJOLS <jsavajols@gmail.com>
2024-08-06 17:49:44 +0200

    connect with another database

--- functions/database/database.go
+++ functions/database/database.go
@@ -9,8 +9,11 @@ import (
 
 // ConnectDatabase connecte la base de données
 func ConnectDatabase(database string) (*sql.DB, error) {
-	if os.Getenv("DB_NAME") != "" {
-		database = os.Getenv("DB_NAME")
+	// Force l'utilisation de la database passée en paramètre si elle est définie
+	if database == "" {
+		if os.Getenv("DB_NAME") != "" {
+			database = os.Getenv("DB_NAME")
+		}
 	}
 	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")+")/"+database)
 	if err != nil {
